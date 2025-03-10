3f70b9cb9e9e193a7e170ef1d558dd5b4687043a
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-03-10 16:12:40 -0300

    Add componente Logo

---
+++ src/components/Logo/index.js
@@ -0,0 +1,15 @@
+import logo from "../../image/logo.svg";
+import "./style.css";
+
+function Logo() {
+  return (
+    <div className="logo">
+      <img src={logo} className="logo-img" alt="logo" />
+      <p>
+        <strong> Clube</strong> dos Livros
+      </p>
+    </div>
+  );
+}
+
+export default Logo;
