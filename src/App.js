750c0c8e017e695d5c4a4abe534fea79d4c76b54
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-03-10 15:38:51 -0300

    Reorganizando os arquivos

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import logo from './imagens/logo.svg'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>   
+        <div className='logo'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>  Clube dos Livros</p>
+          
+
+        </div>   
+      </header>
+    </div>
+  );
+}
+
+export default App;
