6edbb9c4efd1df84f4b3942f128bf34916e5f1be
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-03-11 11:00:12 -0300

    Criando o menu do site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import './App.css';
-import Logo from './components/Logo'
+import "./App.css";
+import Logo from "./components/Logo";
+import IconePerfil from "./image/perfil.svg"
+import IconeSacola from "./image/sacola.svg"
 
+
+const textoOpcoes = ["Categorias", "Favoritos", "Minha Estante"];
+const icones = [IconePerfil, IconeSacola]
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'> 
+    <div className="App">
+      <header className="App-header">
         <Logo />
+
+        <ul className="menu">
+          {textoOpcoes.map((texto) => {
+            return ( <li className="opçãomenu"> <p>{texto}</p> </li>);
+          })}
+        </ul>
+
+        <ul className="icones">
+          {icones.map((icone) => {
+            return ( <li className="icone"> <img src={icone} alt="icone" /> </li>);
+          })}
+
+        </ul>
       </header>
     </div>
   );
