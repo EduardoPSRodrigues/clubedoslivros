08094dc0696f6247d560b6b2d19760a02862350e
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-03-11 16:02:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import "./App.css";
 import Header from "./components/Header";
+import styled from "styled-components";
+
+// Crio um container para o elemento que desejo estilizar que nesse caso é a div
+// OBS: Não utiliza mais a classe para fazer referencia ao elemento
+const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
+
+  li {
+    list-style: none;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer> 
 
         <Header />
-        
-    </div>
+
+    </AppContainer>
   );
 }
 
