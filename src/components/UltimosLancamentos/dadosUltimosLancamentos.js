a2474316d5eb1d0bd7b8388ca2067bb070d18979
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-03-12 18:14:57 -0300

    Criando dados dos ultimos lançamentos

---
+++ src/components/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,8 @@
+import livro from '../../image/livro.png'
+
+export const livros = [
+    { nome: 'Liderança em Design', id: 1, src:livro},
+    { nome: 'Liderança em UX', id: 2, src: livro },
+    { nome: 'Dom Casmurro', id: 3, src: livro },
+  ];
+  
